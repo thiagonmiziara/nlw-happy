4fbedc063a59720edea7702880671ca300a55e39
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-14 19:08:57 -0300

    finalizado mais um episodio

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -30,5 +30,53 @@ map.on('click', (event) => {
 
 // add campo de fotos
 function addPhotoField() {
-    console.log("funciona");
+    //pegar container de fotos #images
+    const container = document.querySelector('#images');
+    //pegar container para duplicar .new-images
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+    //realizar o clone da ultima imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+    // verificar se o campo esta vazio se sim não add ao container de imagens
+    const input = newFieldContainer.children[0];
+    if (input.value == "") {
+        return
+    }
+    //limpar o campo antes de adicionar o container de imagens
+    input.value = '';
+
+    // adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer);
+}
+
+function deleteField(event) {
+    const span = event.currentTarget;
+
+    const fieldsContainer = document.querySelectorAll('.new-upload');
+    if (fieldsContainer.length <= 1) {
+        // limpar o valor do campo
+        span.parentNode.children[0].value = "";
+        return
+    }
+    //deletar o campo
+    span.parentNode.remove();
+
+}
+
+//tseleção do sim e não
+
+function toggleSelect(event) {
+    //retirar a class .active(dos botes)
+    document.querySelectorAll('.button-select button')
+        .forEach(button => button.classList.remove('active'));
+
+    //colocar a class .active nesse botao clicado
+    const button = event.currentTarget;
+    button.classList.add('active');
+
+    // atualizar o meu input hidden com o valor seleionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    //verificar se sim ou não
+    input.value = button.dataset.value
+
 }
