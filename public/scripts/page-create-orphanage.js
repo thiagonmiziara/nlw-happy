c3175bd4de9cc665fe2569ece1bcfb88b1da3236
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-14 17:19:42 -0300

    refatorando css

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,34 @@
+//create map
+const map = L.map('mapid').setView([-18.9339068, -48.2383719], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+let marker;
+// create and add markers
+map.on('click', (event) => {
+
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    // remover icon
+    marker && map.removeLayer(marker);
+    // add icon layer
+    marker = L.marker([lat, lng], { icon }).addTo(map);
+});
+
+// add campo de fotos
+function addPhotoField() {
+    console.log("funciona");
+}
