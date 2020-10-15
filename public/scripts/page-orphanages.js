0faf506693570ff07a1e159c102d4cef51ac0e25
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-15 17:16:41 -0300

    iniciando back-end

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -4,27 +4,43 @@ const map = L.map('mapid').setView([-18.9339068, -48.2383719], 15);
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
 
-
-
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 });
 
 
-//create popup orverlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeigth: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a> ');
+function addMarker({ id, name, lat, lng }) {
 
-//create and add marker
-L
-    .marker([-18.9339068, -48.2383719], { icon })
-    .addTo(map)
-    .bindPopup(popup);
+    //create popup orverlay{{this.id}}
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeigth: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg" > </a>`);
+
+    //create and add marker
+    L
+        .marker([lat, lng], { icon })
+        .addTo(map)
+        .bindPopup(popup);
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+orphanagesSpan.forEach(span => {
+
+    const orphanage = {
+
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
---
+++ src/database/fakedata.js
@@ -0,0 +1,60 @@
+module.exports = [{
+        id: 1,
+        lat: "-18.9339068",
+        lng: "-48.2383719",
+        name: "Lar das meninas",
+        desciption: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1595967783875-c371f35d8049?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1587155829644-b483b36bf5a0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597385573259-d2d9f0b7135d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1600720665043-3c088759e359?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            " https://images.unsplash.com/photo-1594269146507-03861ba52e8d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+
+        opening_hours: "Horário de visitas Das 18h até 8h",
+
+        open_on_weekends: "1"
+
+
+
+    },
+    {
+        id: 2,
+        lat: "-18.9339068",
+        lng: "-48.2483719",
+        name: "Lar dos meninos",
+        desciption: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1595967783875-c371f35d8049?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1602571833724-984f81111ce1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1587155829644-b483b36bf5a0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597385573259-d2d9f0b7135d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1600720665043-3c088759e359?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            " https://images.unsplash.com/photo-1594269146507-03861ba52e8d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+
+        opening_hours: "Horário de visitas Das 18h até 8h",
+
+        open_on_weekends: "0"
+
+    }
+
+
+
+]
